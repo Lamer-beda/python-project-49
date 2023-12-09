29cbfec64e8b7b6a857a2d4652771a9713eb6a29
lamer <you@example.com>
2023-12-09 19:29:02 +0300

    6 step lint

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -4,52 +4,55 @@
 import prompt
 import random
 
-def calculator():
+
+def calcul():
     name = prompt.string('May I have your name? ')
     print('Hello, ' + name + '!')
     count = 0
-    operations=['+', '-', '*']
-    random_operation = random.choice(operations)
     print('What is the result of the expression?')
     while count != 3:
-        operations=['+', '-', '*']
-        random_operation = random.choice(operations)
-        rand_num_1=random.randint(10,50)
-        rand_num_2=random.randint(1,10)
-        print('Question: ' + str(rand_num_1) + random_operation + str(rand_num_2))
+        operations = ['+', '-', '*']
+        rand_operation = random.choice(operations)
+        rand_num1 = random.randint(10, 50)
+        rand_num2 = random.randint(1, 10)
+        print(f"Question: {str(rand_num1)} {rand_operation} {str(rand_num2)}")
         answer = prompt.string('Your answer: ')
-        if random_operation == '+':
-            if rand_num_1 + rand_num_2 == int(answer):
-                print('Correct!')
-                count +=1
-            else:
-                print ("'" + answer + "' is wrong answer ;(. Correct answer was '" + str(rand_num_1 + rand_num_2) + "'\nLet's try again, "+name+"!")
-                break
-        if random_operation == '-':
-            if rand_num_1 - rand_num_2 == int(answer):
-                print('Correct!')
-                count +=1
-            else:
-                print ("'" + answer + "' is wrong answer ;(. Correct answer was '" + str(rand_num_1 - rand_num_2) + "'\nLet's try again, "+name+"!")
-                break
-        if random_operation =='*':
-            if rand_num_1 * rand_num_2 == int(answer):
-                print('Correct!')
-                count +=1
-            else:
-                print ("'" + answer + "' is wrong answer ;(. Correct answer was '" + str(rand_num_1 * rand_num_2) + "'\nLet's try again, "+name+"!")
-                break
-    if count ==3:
-            print('Congratulations, ' + name + '!')
-
+        match rand_operation:
+            case '+':
+                if rand_num1 + rand_num2 == int(answer):
+                    print('Correct!')
+                    count += 1
+                else:
+                    print(f"'{answer}' is wrong answer ;(." +
+                    f"Correct answer was '{str(rand_num1 + rand_num2)}'\n" +
+                    f"Let's try again, {name}!")
+                    break
+            case '-':
+                if rand_num1 - rand_num2 == int(answer):
+                    print('Correct!')
+                    count += 1
+                else:
+                    print(f"'{answer}' is wrong answer ;(." +
+                    f"Correct answer was '{str(rand_num1 - rand_num2)}'\n" +
+                    f"Let's try again, {name}!")
+                    break
+            case '*':
+                if rand_num1 * rand_num2 == int(answer):
+                    print('Correct!')
+                    count += 1
+                else:
+                    print(f"'{answer}' is wrong answer ;(." +
+                    f"Correct answer was '{str(rand_num1 * rand_num2)}'\n" +
+                    f"Let's try again, {name}!")
+                    break
+    if count == 3:
+        print(f"Congratulations, {name}!")
 
 
 def main():
     print('Welcome to the Brain Games!')
-    calculator()
+    calcul()
 
 
 if __name__ == '__main__':
     main()
-
-#
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -11,21 +11,23 @@ def numbers():
     count = 0
     print('Answer "yes" if the number is even, otherwise answer "no".')
     while count != 3:
-        random_number=random.randint(1,100)
+        random_number = random.randint(1, 100)
         print('Question: ' + str(random_number))
         answer = prompt.string('Your answer: ')
         if random_number % 2 == 0 and answer == 'yes':
-            print ('Correct!')
+            print('Correct!')
             count += 1
         elif random_number % 2 != 0 and answer == 'no':
-            print ('Correct!')
+            print('Correct!')
             count += 1
         else:
             if answer == 'yes':
-                print("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again," + name + "!")
+                print("'yes' is wrong answer ;(. Correct answer was 'no'.\n
+                        Let's try again," + name + "!")
                 break
             else:
-                 print("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again," + name + "!")
+                 print("'no' is wrong answer ;(. Correct answer was 'yes'.\n
+                         Let's try again," + name + "!")
                  break
     if count == 3:
         print('Congratulations,' + name + '!')
@@ -38,4 +40,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
