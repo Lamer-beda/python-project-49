1c95f0bdd7a5477398ca2067ecf796ff65fe4715
lamer <you@example.com>
2023-12-10 17:42:02 +0300

    8 step lint

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -11,20 +11,20 @@ def progress():
     count = 0
     print('What number is missing in the progression?')
     while count != 3:
-        l_progress = random.randint(5, 10)
-        first_element = random.randint(1, 50)
-        difference = random.randint(1, 10)
-        progression = [first_element + (i - 1) * difference for i in range(1, l_progress + 1)]
-        index = random.randint(0, len(progression)-1)
+        l_prog = random.randint(5, 10)
+        f_elem = random.randint(1, 50)
+        diff = random.randint(1, 10)
+        progression = [f_elem + (i - 1) * diff for i in range(1, l_prog + 1)]
+        index = random.randint(0, len(progression) - 1)
         answer_game = progression[index]
         progression[index] = '..'
-        print("Question: ", end='')  
+        print("Question: ", end='')
         for num in progression:
-            print(num, end =' ')
+            print(num, end=' ')
         answer = prompt.string('\nYour answer: ')
         if answer_game == int(answer):
             print('Correct!')
-            count +=1
+            count += 1
         else:
             print(f"'{answer}' is wrong answer ;(." +
                 f"Correct answer was '{answer_game}'\n" +
@@ -41,4 +41,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
